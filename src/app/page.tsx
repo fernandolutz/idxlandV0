14602d5af511f424720c826537525b8a1ef40bfa
Fernando Lutz <fernandolutz@gmail.com>
2025-12-11 02:39:51 +0000

    a pagina precisa ser adaptativa para qualquer resoluçao, em um navegador

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-background">
       <Header />
-      <main className="flex-1">
+      <main className="flex-1 w-full">
         <Hero />
         <Solutions />
         <Highlights />
