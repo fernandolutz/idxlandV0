f5096c9e0b122636b447cc65c1bce11c32c56f99
Fernando Lutz <fernandolutz@gmail.com>
2025-12-11 02:30:09 +0000

    deixa a página centralizada

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ import Footer from "@/components/footer";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-background">
+    <div className="flex flex-col min-h-screen bg-background items-center">
       <Header />
       <main className="flex-1">
         <Hero />
