dbab5f795f0d120d7421560ad1e21a9e8e7fd840
Fernando Lutz <fernandolutz@gmail.com>
2025-12-11 02:37:00 +0000

    Página nao está centralizada.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen bg-background">
       <Header />
-      <main className="flex-1 w-full">
+      <main className="flex-1">
         <Hero />
         <Solutions />
         <Highlights />
