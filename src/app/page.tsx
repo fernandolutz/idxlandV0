026a441865f24e1e451bc5a112eb64c4ed0619b3
Fernando Lutz <fernandolutz@gmail.com>
2025-12-11 02:32:52 +0000

    novamente centralize e organize a página para que fique visualmente agra

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import Footer from "@/components/footer";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-background items-center">
+    <div className="flex flex-col min-h-screen bg-background">
       <Header />
       <main className="flex-1 w-full">
         <Hero />
