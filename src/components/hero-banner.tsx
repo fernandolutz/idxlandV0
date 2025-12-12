872351caeb28e295fa3e5eddd3b7c9d035160fcd
Fernando Lutz <fernandolutz@gmail.com>
2025-12-12 01:19:59 +0000

    quando o site é apresentado no modo celular a logo some conforme imagem,

--- src/components/hero-banner.tsx
+++ src/components/hero-banner.tsx
@@ -13,8 +13,8 @@ export default function HeroBanner() {
       <div className="absolute inset-0 bg-primary/80"></div>
       
       <div className="container relative z-10">
-        <div className="flex flex-col md:flex-row items-center justify-between min-h-[calc(100vh-56px)] md:min-h-0 md:h-[550px] py-12 md:py-0">
-          <div className="md:w-1/2 text-center md:text-left">
+        <div className="flex flex-col items-center justify-center text-center min-h-[calc(100vh-56px)] py-20 md:flex-row md:justify-between md:text-left md:h-[550px] md:py-0">
+          <div className="md:w-1/2">
             <h1 className="text-4xl font-bold tracking-tight sm:text-5xl md:text-6xl font-headline">
               Qual o futuro da TI da sua empresa?
             </h1>
@@ -30,8 +30,8 @@ export default function HeroBanner() {
               </Button>
             </div>
           </div>
-          <div className="hidden md:flex md:w-1/2 justify-center items-center relative mt-12 md:mt-0">
-            <div className="relative w-96 h-96 text-white">
+          <div className="w-full max-w-sm mt-16 md:w-1/2 md:mt-0 flex justify-center items-center relative">
+            <div className="relative w-72 h-72 sm:w-96 sm:h-96 text-white">
                 <div 
                     className="absolute inset-0 bg-white/20 rounded-full blur-3xl"
                     style={{ filter: 'blur(80px)' }}
