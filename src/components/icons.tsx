31a0910e05fb5f35292206702b94d1f4bdb7627c
Fernando Lutz <fernandolutz@gmail.com>
2025-12-12 01:10:35 +0000

    a logo da empresa quebrou use a q esta em anexo dessa mensagem

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,15 +4,15 @@ export function IndexDigitalLogo(props: SVGProps<SVGSVGElement>) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 24 24"
+      viewBox="0 0 160 90"
       width="1em"
       height="1em"
       {...props}
     >
-      <path
-        d="M3 3h18v18H3V3zm2 2v14h14V5H5zm2 2h2v10H7V7zm4 0h2v10h-2V7zm4 0h2v10h-2V7z"
-        fill="currentColor"
-      />
+      <g fill="currentColor">
+        <path d="M65.3,1.6h-4.7v3.2h-3.2v3.2H54.2v3.2H51v3.2h-3.2v4.7H44.6v3.2h-3.2v3.2h-3.2v3.2h-3.2v3.2h3.2v3.2h3.2v3.2h3.2v3.2h3.2v4.7h4.7v3.2h3.2v3.2h3.2v3.2h3.2V51h3.2v-3.2h3.2v-3.2h3.2v-3.2h3.2V36h4.7v-3.2h3.2v-3.2h3.2V26h3.2v-3.2h-3.2v-3.2h-3.2v-3.2h-3.2v-3.2h-3.2v-4.7h-4.7v-3.2h-3.2V4.8h-3.2V1.6Zm-1.5,6.4h1.5v1.6h1.6v1.6h1.6v1.6h1.6v1.6h-1.6v-1.6h-1.6v-1.6h-1.6V8Zm-19.1,0h1.5v1.6h1.6v1.6h1.6v1.6h1.6v1.6h-1.6v-1.6h-1.6v-1.6h-1.6V8Zm-6.4,19.1h1.6v-1.5h1.6v-1.6h1.6v-1.6h1.6v-1.6h-1.6v1.6h-1.6v1.6h-1.6v1.6h-1.6v1.6Zm0,19.1h1.6v-1.5h1.6v-1.6h1.6v-1.6h1.6v-1.6h-1.6v1.6h-1.6v1.6h-1.6v1.6h-1.6v1.6Zm25.5,12.7h-1.5v-1.6h-1.6v-1.6h-1.6v-1.6h-1.6v-1.6h1.6v1.6h1.6v1.6h1.6v1.6h1.6v1.6ZM80,27.1h-1.6v1.5h-1.6v1.6h-1.6v1.6h-1.6v1.6h1.6v-1.6h1.6v-1.6h1.6v-1.6h1.6v-1.6ZM51,20.7h25.5v19.1H51V20.7Zm22.3,15.9V23.9H54.2v12.7h19.1Z" />
+        <text x="20" y="75" fontFamily="sans-serif" fontSize="20" fontWeight="bold">Index TI</text>
+      </g>
     </svg>
   );
 }
